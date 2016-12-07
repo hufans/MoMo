0086ecb382dccf920a97ab502f17c8c01bb1d3b1
胡凡 <hufan@hufandemacbook-pro.local>
2016-12-07 12:43:30 +0800

    Init Commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform:ios,'7.0'
+target 'The Other' do
+
+pod 'AFNetworking'
+pod 'MBProgressHUD'
+pod 'MJRefresh'
+pod 'YYModel'
+
+end
---
+++ Pods/Target Support Files/Pods-The Other/Pods-The Other-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_The_Other : NSObject
+@end
+@implementation PodsDummy_Pods_The_Other
+@end
